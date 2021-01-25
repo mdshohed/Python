8d0b3f3f1926a266ba3061c139d4d3c966ff2563
Shohedul Islam <49224753+mdshohed@users.noreply.github.com>
2021-01-25 21:14:26 +0600

    Update and rename PyAutoGUI to PyAutoGUI.py

---
+++ Python frameworks/PyAutoGUI.py
@@ -0,0 +1,17 @@
+#auto message: 
+
+
+import pyautogui
+import time
+
+i = 10
+while i>0:
+    time.sleep(1)
+    pyautogui.typewrite("Hello")
+    time.sleep(1)
+    pyautogui.press("enter")
+    i = i-1
+    
+    
+
+Install Link: (https://pypi.org/project/PyAutoGUI)
