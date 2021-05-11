9b38a37fac7a27493e58af4097d561fbb4a51265
Shohedul Islam <49224753+mdshohed@users.noreply.github.com>
2021-05-11 16:06:18 +0600

    Create 2: If - else.py

---
+++ 2: If - else.py
@@ -0,0 +1,30 @@
+# Mark calculate
+mark = int(input("Enter exam mark: "))
+
+if mark >= 80:
+    print("A+")
+elif mark >= 75:
+    print("A-")
+elif mark >= 70:
+    print("A")
+elif mark >= 65:
+    print("B+")
+elif mark >= 60:
+    print("B-")
+elif mark >= 55:
+    print("B")
+elif mark >= 50:
+    print("B-")
+elif mark >= 45:
+    print("C")
+elif mark >= 40:
+    print("D")
+else:
+    print("F")
+
+# even odd
+value = int(input("Enter the value: "))
+if value % 2 == 0:
+    print("Even\n")
+else:
+    print("Odd\n")
