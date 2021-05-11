92e7132b78c9e3d981c4f8b787b24b5108e54dcc
Shohedul Islam <49224753+mdshohed@users.noreply.github.com>
2021-05-11 16:35:41 +0600

    Update Python5: Arrays.py

--- Python5: Arrays.py
+++ Python5: Arrays.py
@@ -1,1 +1,47 @@
 
+# list int value input
+list = []
+n = int(input())
+for i in range(0, n):
+    t = int(input())
+    list.append(t)
+print(list)
+
+# list str value input
+list2 = []
+p = int(input())
+for i in range(0, p):
+    t = str(input())
+    list2.append(t)
+print(list2)
+
+# 2d matrix represent
+matrix = [[1, 3, 4, 5], [2, 4, 6, 8], [1, 5, 9, 2],[5, 9, 54, 23]]
+for i in matrix:
+    for j in i:
+        print(j)
+    print()
+
+for i in matrix:
+    for j in i:
+        print(j, end=' ')
+    print()
+
+print(matrix[0:2]);
+
+# ara search found after that new string add
+ara = ["mdshohed", "hello", "tusar", "anti"];
+for car in ara:
+    print(car)
+    if car == "hello":
+        print("hello mutu")
+
+# sum calculate int or not
+
+
+list = [2, 3, 4, "mdshohed", 5]
+sum = 0
+for i in list:
+    if type(i) == int:
+        sum += i
+print("sum is : {sum}".format(sum=sum))
