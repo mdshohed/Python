6dcefa7df0a43556c7900d32e51850890fc52702
Shohedul Islam <49224753+mdshohed@users.noreply.github.com>
2021-05-11 15:56:58 +0600

    Create 1: Input & Output.py

---
+++ 1: Input & Output.py
@@ -0,0 +1,15 @@
+
+int_value  = int(input("Integer value: "))
+print(int_value)
+
+float_value = float(input("Float value: "))
+print(float_value)
+
+string_value = input("Enter you name: ")
+print(string_value)
+
+
+# 20 + 30 = 50
+val1 = 20
+val2 = 30
+print( f"{val1} + {val2} = {val1+val2}")
