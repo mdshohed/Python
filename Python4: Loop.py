53bb9b7e9dd8f02799e6e2fd379498c7f4c762e9
Shohedul Islam <49224753+mdshohed@users.noreply.github.com>
2021-05-11 16:34:31 +0600

    Update Python4: Loop.py

--- Python4: Loop.py
+++ Python4: Loop.py
@@ -1,1 +1,18 @@
 
+
+# For Loop
+n = 10
+for i in range(3 , n):
+    print(i)
+
+
+# While Loop
+n = 10
+i = 0
+while i<= n:
+    if i == 5:
+        continue
+    print(i)
+    i += 1
+    if i == 8:
+        break
