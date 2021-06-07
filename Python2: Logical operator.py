53ca404d54f540645a4cca26c34b1bd4cb0b41e0
Shohedul Islam <49224753+mdshohed@users.noreply.github.com>
2021-06-07 18:06:29 +0600

    Update Python2: Logical operator.py

--- Python2: Logical operator.py
+++ Python2: Logical operator.py
@@ -5,7 +5,7 @@ n = int(input("Enter the value 1: "))
 m = int(input("Enter the value 2: "))
 
 if  n>=0 and m>=0: 
-    print( "Greate then 0")
+    print( "Greater then 0")
 elif n<=0 and m<=0: 
     print("less then 0")
 elif n>=0 or m>=0: 
